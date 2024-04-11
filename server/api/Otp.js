5b1da70bbc1710a3a2a6f3e3ca570cf99a1c5799
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-11 13:16:06 +0530

    Update Otp.js

--- server/api/Otp.js
+++ server/api/Otp.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+
+const Authenticate = require('../middleware/authentication.js');
+
 //getting User 
 
 const User = require('../model/User')
