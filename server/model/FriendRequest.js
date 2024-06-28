c3fdb596f3a361db40c2afef81b8a55de8368281
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-06-29 00:29:14 +0530

    Update FriendRequest.js

--- server/model/FriendRequest.js
+++ server/model/FriendRequest.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const friendRequestSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'UserData', required: true },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'UserData', required: true },
-  status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
+  imageUrl: { type: String,  required: true  }, // Make sure imageUrl is required if necessary
+  status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' },
+  timestamp: { type: Date, default: Date.now }
 });
 
 const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
